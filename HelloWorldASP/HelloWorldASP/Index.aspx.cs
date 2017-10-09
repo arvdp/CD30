b3cb5bd19a09c20122a9133f7462d84b79214c13
Arjen van der Pas <a.vanderpas@bkr.nl>
2017-10-09 21:03:28 +0200

    HelloWorldASP toegevoegd

---
+++ HelloWorldASP/HelloWorldASP/Index.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorldASP
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "WORLD";
+            // TODO Aanroepen van HelloWorld.Hi.result
+        }
+    }
+}
